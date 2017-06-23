93d4d837197344bab95633c60c3c09465789112c
laerciomedeiros <laerciomedeiros2015@gmail.com>
2017-06-23 11:27:12 -0300

    fiz o primeiro teste.

--- Teste.java
+++ Teste.java
@@ -3,14 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.algas;
 
 /**
  *
  * @author VILA
  */
-public class ClasseTeste {
-    public static void main(String[] args){
-        
-    }
+public class Teste {
+    
 }
